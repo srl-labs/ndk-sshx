1f2bcb7aaf803b1f550c167b8556c59e2a23d0f2
Roman Dodin <dodin.roman@gmail.com>
2024-10-05 21:34:00 +0300

    added enum state update and yang descriptions

--- app/state.go
+++ app/state.go
@@ -2,10 +2,14 @@ package app
 
 import (
 	"encoding/json"
+	"fmt"
 )
 
 // updateState updates the state of the application.
 func (a *App) updateState() {
+	// enum values should be provided in the form of "<ENUM-NAME>_VALUE"
+	a.configState.Shell = fmt.Sprintf("SHELL_%s", a.configState.Shell)
+
 	jsData, err := json.Marshal(a.configState)
 	if err != nil {
 		a.logger.Info().Msgf("failed to marshal json data: %v", err)
